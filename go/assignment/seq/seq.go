1a1f5fc5cf8b504324e74aad3eb216a9f23f0d65
Duncan Paul Attard <duncanatt@gmail.com>
2017-10-14 00:11:57 +0200

    Adding sequential package skeleton and its test suite.

---
+++ go/assignment/seq/seq.go
@@ -0,0 +1,103 @@
+/// ----------------------------------------------------------------------------
+/// @author Duncan Paul Attard
+///
+/// Model answers for worksheet 1 questions.
+/// ----------------------------------------------------------------------------
+package util
+
+// -----------------------------------------------------------------------------
+// Computes factorial numbers using direct recursion.
+// func Fact(n) where:
+//   * n is the number whose factorial is to be computed.
+// Returns: The factorial for index n.
+// -----------------------------------------------------------------------------
+func Fact(n int) int {
+  // TODO: Add implementation.
+}
+
+// -----------------------------------------------------------------------------
+// Computes factorial numbers using indirect recursion.
+// func Fact2(n) where:
+//   * n is the number whose factorial is to be computed.
+// Returns: The factorial for index n.
+// -----------------------------------------------------------------------------
+func Fact2(n int, acc int) int {
+  return Fact3(n, 1)
+}
+
+func Fact3(n int, acc int) int {
+  // TODO: Add implementation.
+}
+
+// -----------------------------------------------------------------------------
+// Computes fibonacci numbers using direct recursion.
+// func Fib(n) where:
+//   * n is the number whose fibonacci number is to be computed.
+// Returns: The fibonacci number for index n.
+// -----------------------------------------------------------------------------
+func Fib(n int) int {
+  // TODO: Add implementation.
+}
+
+// -----------------------------------------------------------------------------
+// Computes fibonacci numbers using indirect recursion.
+// func Fib2(n) where:
+//   * n is the number whose fibonacci number is to be computed.
+// Returns: The fibonacci number for index n.
+// -----------------------------------------------------------------------------
+func Fib2(n int) int {
+  return Fib3(n, 0, 1)
+}
+
+func Fib3(n int, n_minus2 int, n_minus1 int) int {
+  // TODO: Add implementation.
+}
+
+// -----------------------------------------------------------------------------
+// Maps an array of elements into another array of elements having equal length.
+// func Map(f, elems) where:
+//   * f is a function that maps the current element x in the array to a new
+//     value.
+//   * elems is the array to map.
+// Returns: The same array with f applied on each of its elements.
+// -----------------------------------------------------------------------------
+func Map(f func(int) int, elems []int) []int {
+  // TODO: Add implementation.
+}
+
+// -----------------------------------------------------------------------------
+// Filters an array of elements according to the specifed predicate f.
+// func Filter(f, elems) where:
+//   * f is a function that tests the current element x in the array and returns
+//     a boolean, indicating whether x is to be retained.
+//   * elems is the array to filter.
+// Returns: A new array filtered according to f, possibly containing fewer
+//          elements than the original array.
+// -----------------------------------------------------------------------------
+func Filter(f func(int) bool, elems []int) []int {
+  // TODO: Add implementation.
+}
+
+// -----------------------------------------------------------------------------
+// Reduces an array of elements using the specified combining function f,
+// starting with the seed value.
+// func Reduce(f, seed, elems) where:
+//   * f is a function that combines the current element x in the array with the
+//      next element x + 1 in the array.
+//   * seed is the seed that the reduction starts with.
+//   * elems is the array to reduce.
+// Returns: A single value that reflects the reduction of elems.
+// -----------------------------------------------------------------------------
+func Reduce(f func(x int, acc int) int, seed int, elems []int) int {
+  // TODO: Add implementation.
+}
+
+// -----------------------------------------------------------------------------
+// Reverses the specified list.
+// func Reverse(elems) where:
+//   * elems is the array to be reversed.
+// Returns: A new array that is the reverse of elems.
+// -----------------------------------------------------------------------------
+func Reverse(elems []int) []int {
+  // TODO: Add implementation.
+}
---
+++ go/assignment/seq/seq_test.go
@@ -0,0 +1,166 @@
+/// ----------------------------------------------------------------------------
+/// @author Duncan Paul Attard
+///
+/// Provides a batch of unit tests for worksheet 1.
+///
+/// To run tests, use 'go test -v' on the command line from the module's
+/// directory.
+/// ----------------------------------------------------------------------------
+package util
+
+import (
+  "testing"
+  "fmt"
+  "../util"
+  )
+
+// -----------------------------------------------------------------------------
+// Tests the Fact functionality.
+// -----------------------------------------------------------------------------
+func TestFact(t *testing.T) {
+  res := Fact(10)
+  fmt.Println("Fact test:", res)
+  util.AssertEqual(t, 3628800, res, "")
+}
+
+// -----------------------------------------------------------------------------
+// Tests the Fact2 functionality.
+// -----------------------------------------------------------------------------
+func TestFact2(t *testing.T) {
+  res := Fact(10)
+  fmt.Println("Fact2 test:", res)
+  util.AssertEqual(t, 3628800, res, "")
+}
+
+// -----------------------------------------------------------------------------
+// Tests the Fib functionality.
+// -----------------------------------------------------------------------------
+func TestFib(t *testing.T)  {
+  res := Fib(10)
+  fmt.Println("Fib test:", res)
+  util.AssertEqual(t, 89, res, "")
+}
+
+// -----------------------------------------------------------------------------
+// Tests the Fib2 functionality.
+// -----------------------------------------------------------------------------
+func TestFib2(t *testing.T) {
+  res := Fib(10)
+  fmt.Println("Fib2 test:", res)
+  util.AssertEqual(t, 89, res, "")
+}
+
+// -----------------------------------------------------------------------------
+// Tests the Map functionality.
+// -----------------------------------------------------------------------------
+func TestMapEmpty(t *testing.T) {
+  ints := []int{}
+  double := func(x int) int {
+    return x * 2
+  }
+
+  res := Map(double, ints)
+  fmt.Println("Map test with empty list:", res)
+  util.AssertEqual(t, 0, len(res), "")
+}
+
+// -----------------------------------------------------------------------------
+// Tests the Map functionality.
+// -----------------------------------------------------------------------------
+func TestMap(t *testing.T) {
+  ints := []int{1, 2, 3, 4}
+  double := func(x int) int {
+    return x * 2
+  }
+
+  exp := []int{2, 4, 6, 8}
+  res := Map(double, ints)
+  fmt.Println("Map test with non-empty list:", res)
+  for i, r := range res {
+    util.AssertEqual(t, exp[i], r, "")
+  }
+}
+
+// -----------------------------------------------------------------------------
+// Tests the Filter functionality.
+// -----------------------------------------------------------------------------
+func TestFilterEmpty(t *testing.T) {
+  ints := []int{}
+  is_natural := func(x int) bool {
+    return x >= 0
+  }
+
+  res := Filter(is_natural, ints)
+  fmt.Println("Filter test with empty list:", res)
+  util.AssertEqual(t, 0, len(res), "")
+}
+
+// -----------------------------------------------------------------------------
+// Tests the Filter functionality.
+// -----------------------------------------------------------------------------
+func TestFilter(t *testing.T) {
+  ints := []int{1, -2, 3, 4}
+  is_natural := func(x int) bool {
+    return x >= 0
+  }
+
+  exp := []int{1, 3, 4}
+  res := Filter(is_natural, ints)
+  fmt.Println("Filter test with non-empty list:", res)
+  for i, r := range res {
+    util.AssertEqual(t, exp[i], r, "")
+  }
+}
+
+// -----------------------------------------------------------------------------
+// Tests the Reduce functionality.
+// -----------------------------------------------------------------------------
+func TestReduceEmpty(t *testing.T) {
+  ints := []int{}
+  add := func(x int, acc int) int {
+    return x + acc
+  }
+
+  res := Reduce(add, 0, ints)
+  fmt.Println("Reduce test with empty list:", res)
+  util.AssertEqual(t, 0, res, "")
+}
+
+// -----------------------------------------------------------------------------
+// Tests the Reduce functionality.
+// -----------------------------------------------------------------------------
+func TestReduce(t *testing.T) {
+  ints := []int{1, 2, 3, 4}
+  add := func(x int, acc int) int {
+    return x + acc
+  }
+
+  res := Reduce(add, 0, ints)
+  fmt.Println("Reduce test with non-empty list:", res)
+  util.AssertEqual(t, 10, res, "")
+}
+
+// -----------------------------------------------------------------------------
+// Tests the Reverse functionality.
+// -----------------------------------------------------------------------------
+func TestReverseEmpty(t *testing.T) {
+  ints := []int{}
+
+  res := Reverse(ints)
+  fmt.Println("Reverse test with empty list:", res)
+  util.AssertEqual(t, 0, len(res), "")
+}
+
+// -----------------------------------------------------------------------------
+// Tests the Reverse functionality.
+// -----------------------------------------------------------------------------
+func TestReverseNonEmpty(t *testing.T) {
+  ints := []int{1, 2, 3, 4}
+
+  exp := []int{4, 3, 2, 1}
+  res := Reverse(ints)
+  fmt.Println("Reverse test with non-empty list:", res)
+  for i, r := range res {
+    util.AssertEqual(t, exp[i], r, "")
+  }
+}
